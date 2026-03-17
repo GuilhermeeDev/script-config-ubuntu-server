b33489186f30c07737b8fec78e8a3c0aab89d340
GuilhermeeDev <guilhermee.dev.software@gmail.com>
2026-03-17 18:06:45 -0400

    (update) Adicionando flag de instalação automatizada de pacotes

--- docker-install.sh
+++ docker-install.sh
@@ -15,6 +15,5 @@ EOF
 
 sudo apt update
 
-sudo apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
-
+sudo apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
 sudo usermod -a -G docker $USER
